d9c6c3687411b99b5977498995639b919458823a
baus8650 <baus8650@gmail.com>
2022-09-26 16:04:32 -0700

    Add fetch, add, and delete methods to FolderViewModel Update image data type from transformable to Binary Data in core data entity

--- Task Master/View Models/FolderViewModel.swift
+++ Task Master/View Models/FolderViewModel.swift
@@ -5,8 +5,46 @@
 //  Created by Tim Bausch on 9/26/22.
 //
 
+import CoreData
 import Foundation
+import UIKit
 
 final class FolderViewModel {
+    var folders: [Folder] = []
+    
+    init() {
+        getFolders()
+    }
+    
+    public func getFolders() {
+        let folderFetch: NSFetchRequest<Folder> = Folder.fetchRequest()
+        do {
+            let results = try CoreDataStack.shared.container.viewContext.fetch(folderFetch)
+            self.folders = results
+        } catch let error as NSError {
+            print("Fetch error: \(error) description: \(error.userInfo)")
+        }
+    }
+    
+    public func addFolder(name: String, image: UIImage) {
+        guard let imageData = image.pngData() else { return }
+        let newFolder = Folder(context: CoreDataStack.shared.container.viewContext)
+        newFolder.name = name
+        newFolder.image = imageData
+        do {
+            try CoreDataStack.shared.container.viewContext.save()
+        } catch {
+            print("Unable to save Folder \(error)")
+        }
+    }
+    
+    public func deleteFolder(_ folder: Folder) {
+        CoreDataStack.shared.container.viewContext.delete(folder)
+        do {
+            try CoreDataStack.shared.container.viewContext.save()
+        } catch {
+            print("Unable to delete Folder \(error)")
+        }
+    }
     
 }
