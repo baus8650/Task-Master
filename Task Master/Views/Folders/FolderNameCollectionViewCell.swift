bd0cdeba22bfe4e805e7a243d9b2d4aa99e4a2dd
baus8650 <baus8650@gmail.com>
2022-09-27 15:38:46 -0700

    Add combine to FolderNameCollectionViewCell to track changes in textField Add TextFieldDelegate extension to FolderNameCollectionViewCell Replace custom corner radi to .roundedRect border style

--- Task Master/Views/Folders/FolderNameCollectionViewCell.swift
+++ Task Master/Views/Folders/FolderNameCollectionViewCell.swift
@@ -5,25 +5,24 @@
 //  Created by Tim Bausch on 9/27/22.
 //
 
+import Combine
 import PureLayout
 import UIKit
 
 class FolderNameCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
-    var name: String!
+    @Published var name: String!
     
     // MARK: - Views
     private lazy var nameField: UITextField = {
         let textField = UITextField(forAutoLayout: ())
-//        textField.autoSetDimension(.height, toSize: 48)
+        textField.delegate = self
         textField.placeholder = "Folder Name"
         textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
         textField.leftViewMode = .always
-        
         textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
         textField.rightViewMode = .always
-        textField.layer.cornerRadius = 8
-        textField.layer.borderWidth = 1
+        textField.borderStyle = .roundedRect
         return textField
     }()
     
@@ -39,5 +38,18 @@ class FolderNameCollectionViewCell: UICollectionViewCell {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FolderNameCollectionViewCell: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var finalString = ""
+        if range.length > 0 {
+            finalString = "\(textField.text!.dropLast())"
+        } else {
+            finalString = "\(textField.text! + string)"
+        }
+        name = finalString
+        return true
     }
 }
