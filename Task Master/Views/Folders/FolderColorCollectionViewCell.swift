e5dc735ff9042e95dd9028b059b5216f0c102b6d
baus8650 <baus8650@gmail.com>
2022-09-28 16:14:36 -0700

    Add color select button as a colored circle Add hardcoded list of hex values for options

--- Task Master/Views/Folders/FolderColorCollectionViewCell.swift
+++ Task Master/Views/Folders/FolderColorCollectionViewCell.swift
@@ -5,18 +5,50 @@
 //  Created by Tim Bausch on 9/27/22.
 //
 
+import PureLayout
 import UIKit
 
 class FolderColorCollectionViewCell: UICollectionViewCell {
     
-    var color: UIColor = .purple
+    @Published var color: String = "9C80B5"
+    
+    lazy var colorSelectButton: UIButton = {
+        let button = UIButton(forAutoLayout: ())
+        button.addTarget(self, action: #selector(didSelectColor), for: .touchUpInside)
+        return button
+    }()
+    
+    let colorList: [String] = [
+        "9C80B5",
+        "8FF570",
+        "FF6459",
+        "FF9A60",
+        "FFF07F",
+        "6CFAFA",
+        "7AB6FF",
+        "C04AFF",
+        "FD8FFF",
+        "B8B8B8",
+        "C99DB4",
+        "FFD1F2"
+    ]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         layer.cornerRadius = layer.bounds.width / 2
         clipsToBounds = true
-        backgroundColor = color
         
+        setUpSubviews()
+    }
+    
+    private func setUpSubviews() {
+        addSubview(colorSelectButton)
+        colorSelectButton.autoPinEdgesToSuperviewEdges()
+    }
+    
+    @objc
+    private func didSelectColor(_ sender: UIButton) {
+        color = colorList[sender.tag]
     }
 
     required init?(coder aDecoder: NSCoder) {
